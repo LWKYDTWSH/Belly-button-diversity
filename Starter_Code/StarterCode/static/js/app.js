da954cb76564da2fe1d682fcd79108ff2c534e80
LWKYDTWSH <106898202+LWKYDTWSH@users.noreply.github.com>
2023-01-15 14:34:52 -0600

    14.5

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,24 +1,118 @@
-//Begin by creating a global variable to make
-//it easier to access and call data
-var data = {};
-d3.json("./samples.json").then(function (call_data) {
-  console.log(call_data);
-  data = call_data;
-    //Save all created functions here for global variable
-        loadDrop();
-        getDemo("940");
-        });
-function loadDrop() {
-  for (let i = 0; i < data.names.length; i++){
-    let name = data.names[i];
-    d3.select("#selDataset").append("option").text(name);
-  }
+// Belly Button Biodiversity 
+
+//Build the Metadata aka the main
+//use forEach for the keys and values
+//H6 tag is where metadata is found
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata= data.metadata;
+    var resultsarray= metadata.filter(sampleobject => 
+      sampleobject.id == sample);
+    var result= resultsarray[0]
+    var panel = d3.select("#sample-metadata");
+    panel.html("");
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key}: ${value}`);
+    });
+  });
 }
-//Show when the drop down is clicked in Demo info
-function optionChanged(val) {
-    getDemo(val);
-}
-    //Generate the sample-metadata to show in the panel-body
-    //of the demographic info table
-    function getDemo(val) {
+
+function buildCharts(sample) {
+
+// Create callable variable to begin build bubble and bar chart.
+//To get samples we pull data.samples from the main and we will need to apply a filter
+//for sample object to be sample IDs
+d3.json("samples.json").then((data) => {
+  var samples= data.samples;
+  var resultsarray= samples.filter(sampleobject => 
+      sampleobject.id == sample);
+  var result= resultsarray[0]
+
+  var ids = result.otu_ids;
+  var labels = result.otu_labels;
+  var values = result.sample_values;
+
+
+
+//Section for bubble chart
+//Added symbol feature to help the top baterias stand out
+  var LayoutBubble = {
+    margin: { t: 0 },
+    xaxis: { title: "OTU ID" },
+    yaxis: { title: "Sample Size" },
+    hovermode: "closest",
+    };
+
+    var DataBubble = [ 
+    {
+      x: ids,
+      y: values,
+      text: labels,
+      mode: "markers",
+      marker: {
+        symbol: ['circle', 'square', 'diamond', 'cross'],
+        color: ids,
+        size: values,
+        }
     }
+  ];
+
+  Plotly.newPlot("bubble", DataBubble, LayoutBubble);
+
+
+//Begin the bar chart
+//use .slice to return a portion of the array and reverse
+
+  var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      margin: { t: 30, l: 150 },
+    };
+
+  var bar_data =[
+    {
+      y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+      x:values.slice(0,10).reverse(),
+      text:labels.slice(0,10).reverse(),
+      type:"bar",
+      orientation:"h",
+      marker: {color: 'green'}
+
+    }
+  ];
+
+  Plotly.newPlot("bar", bar_data, barLayout);
+});
+}
+ 
+
+function init() {
+// Select within the drop down menu by using the #selDataset
+var selector = d3.select("#selDataset");
+
+// Find the sample name to populate
+d3.json("samples.json").then((data) => {
+  var sampleNames = data.names;
+  sampleNames.forEach((sample) => {
+    selector
+      .append("option")
+      .text(sample)
+      .property("value", sample);
+  });
+
+  // Only pull the first sample for reference when beginning
+  const firstSample = sampleNames[0];
+  buildCharts(firstSample);
+  buildMetadata(firstSample);
+});
+}
+
+function optionChanged(newSample) {
+// Get new sample info each time clicked
+buildCharts(newSample);
+buildMetadata(newSample);
+}
+
+
+
+// Initialize the dashboard
+init();
